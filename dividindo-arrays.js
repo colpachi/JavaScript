2ffeb22c5d1736fd3fcf067d1133b181e96df68d
Murilo Colpachi <murilobcolpachi@gmail.com>
2021-10-09 20:35:59 -0300

    separando alunos em salas, usando o método slice

---
+++ dividindo-arrays.js
@@ -0,0 +1,11 @@
+const nomes = ['João', 'Juliana', 'Ana', 'Caio',
+'Lara', 'Marjorie', 'Guilherme', 'Aline', 'Fabiana',
+'Andre', 'Carlos', 'Paulo', 'Bia', 'Vivian', 'Isabela',
+'Vinicius', 'Renan', 'Renata', 'Daisy', 'Camilo']
+
+const sala1 = nomes.slice(0,nomes.length/2)
+
+const sala2 = nomes.slice(nomes.length/2)
+
+console.log(`componentes da sala 1: ${sala1}`)
+console.log(`componentes da sala 2: ${sala2}`)
