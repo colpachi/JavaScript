1d147ba613255425fd3842113b82e647b8e158a0
Murilo Colpachi <murilobcolpachi@gmail.com>
2021-10-09 19:00:17 -0300

    variables.js file added with some samples of variables utilization

--- variables.js
+++ variables.js
@@ -1,1 +1,17 @@
-ÿþ
+/* Accordingly to MDN a 
+
+Var statement:
+
+declares a function-scoped or globally-scoped variable, optionally initializing it to a value.
+
+JavaScript Hoisting refers to the process whereby the interpreter allocates memory for variable 
+and function declarations prior to execution of the code.
+
+*/
+
+testingVar(); //hoisting example, calling a function before its own declaration.
+
+function testingVar() {
+    var x = 'abc'
+    return console.log(x);
+}
