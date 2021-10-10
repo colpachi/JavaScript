59825b118983b679b4982d119db8bb70bd871ece
Murilo Colpachi <murilobcolpachi@gmail.com>
2021-10-10 19:16:10 -0300

    edit: utilização do método map para criar um array atualizado

--- metodo-map.js
+++ metodo-map.js
@@ -5,7 +5,8 @@ const notas = [10, 9, 8, 7, 6]
 const notasAtualizadas = notas.map(nota => nota == 10 ? nota : ++nota) //função callback que recebe uma arrow function com operador ternário
 
 /* diferentemente do foreach (que não retorna nada, apenas itera entre os itens de um array), 
-o metodo map retorna um array atualizado */
+o metodo map pode retornar um array atualizado se a função informada tiver um return em seu escopo 
+sendo que este array, pode conter qualquer tipo de dado, string, number etc */
 
 console.log(notasAtualizadas)
 
