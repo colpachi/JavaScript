20f8a9ce2f07753f5bed7a00cf005252c2c70b67
Murilo Colpachi <murilobcolpachi@gmail.com>
2021-10-09 22:58:29 -0300

    trabalhando com listas d e duas dimensoes

---
+++ lista-duas-dimensoes.js
@@ -0,0 +1,7 @@
+const alunos = ['Joao', 'Juliana', 'Caio', 'Ana']
+
+const mediasDosAlunos = [10, 7, 9, 6]
+
+let listaDeNotasEAlunos = [alunos, mediasDosAlunos] //lista com duas dimensoes
+
+console.log(`${listaDeNotasEAlunos[0][0]}, sua média é ${listaDeNotasEAlunos[1][0]}.`)
