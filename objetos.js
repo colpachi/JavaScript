94662a788c1858b420969d3c59a268ffd62c287d
Murilo Colpachi <murilobcolpachi@gmail.com>
2021-11-02 15:34:59 -0300

    Exemplo de objetos em JavaScript

---
+++ objetos.js
@@ -0,0 +1,10 @@
+const listaCPFs = ["12345678", "987652211", "372948723"]
+
+//const cliente = ["nome", "André", "idade", 36]
+
+const cliente = {
+    nome:"Andre", 
+    idade:36,
+    cpf:"123456789",
+    email:"andre@email.com"
+}
