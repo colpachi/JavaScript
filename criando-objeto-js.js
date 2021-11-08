fe0d32af9b8caa8888ef3cead0cd889a89a53279
Murilo Colpachi <murilobcolpachi@gmail.com>
2021-11-08 14:40:27 -0300

    Criado arquivo com exemplo de criação de objeto

---
+++ criando-objeto-js.js
@@ -0,0 +1,11 @@
+const objPeter = {
+    primeiroNome:"Peter",
+    sobrenome:"Parker",
+    cpf:"41897464554",
+    corUniforme:"vermelho",
+    superPoder:"teia de aranha",
+    consegueVoar:"não",
+    consegueSaltar:"sim"
+}
+
+console.log(objPeter.cpf)
